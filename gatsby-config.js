ba1347cfddb742e035d464b12f37d5a486a4ae88
Tom Beck <tom@ocupop.com>
2019-09-10 07:11:45 -0700

    added firebase credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,18 @@ module.exports = {
     {
       resolve: '@martinreiche/gatsby-firestore',
       options: {
-        credential: "./firebaseConfig.json",
+        credential: {
+          "type": "service_account",
+          "project_id": process.env.FIREBASE_PROJECT_ID,
+          "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+          "private_key": process.env.FIREBASE_PRIVATE_KEY,
+          "client_email": "firebase-adminsdk-3dxji@p-store-1e62e.iam.gserviceaccount.com",
+          "client_id": process.env.FIREBASE_CLIENT_ID,
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3dxji%40p-store-1e62e.iam.gserviceaccount.com"
+        },
         types: [
           {
             type: 'Product',

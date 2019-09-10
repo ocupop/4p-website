63668c97cfaae45b266db6a833828cf77ffcc743
Tom Beck <tom@ocupop.com>
2019-09-10 05:38:16 -0700

    added file and folder structure to support CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `webpages`,
+        path: `${__dirname}/src/admin/_webpages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-playground`,
---
+++ src/admin/_products/_defaults.md
@@ -0,0 +1,18 @@
+---
+title:
+date:
+handle: default
+type:
+notes_html:
+product_specs_html:
+videos:
+  - label:
+    video:
+instructions:
+  - label:
+    document_path:
+required_products:
+product_faq:
+  - question:
+    answer:
+---

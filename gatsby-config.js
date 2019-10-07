e162457428cb897a7ac017b8ce1ae5b9d6b4faf9
Tom Beck <tom@ocupop.com>
2019-10-07 10:02:17 -0700

    adjust naming convention

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
       options: {
         // credential: './firebaseConfig.json',
         appConfig: {
-          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-          appID: process.env.GATSBY_FIREBASE_APP_ID,
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appID: process.env.FIREBASE_APP_ID,
         },
         types: [
           {

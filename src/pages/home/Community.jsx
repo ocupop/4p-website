f9869a5a18ef2a2dcc3e56dbd245a636ccebdabf
Tom Beck <tom@ocupop.com>
2019-10-14 08:55:10 -0700

    homepage: community section

---
+++ src/pages/home/Community.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+function Community() {
+  return (
+    <section id="bring-us-to-your-community" className="bg-primary text-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-6 mx-lg-auto text-center">
+            <h3>Bring 4p to your community</h3>
+            <p className="lead"><em>Supportive Sub-Headline Here</em></p>
+            <p>4p Foods sources farm-fresh food from farms in the Washington DC local foodshed and delivers them to your office or apartment building once each week.</p>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col-12 col-xl-3 px-5">
+            <div className="row align-items-center mb-5">
+              <div className="col-12 col-md-3 col-xl-12">
+                <div className="p-5 p-md-0 text-center">
+                  <img src="/img/icons/4p-homepage-youroffice.png" className="img-fluid w-75" alt="" />
+                </div>
+              </div>
+              <div className="col-12 col-md-9 col-xl-12 text-xl-center">
+                <h4>Your Office</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione praesentium veritatis at error fuga magni labore cum iusto, natus obcaecati recusandae, fugiat voluptatibus reprehenderit laborum ex magnam molestiae? Sit, omnis!</p>
+                <p><a href="/" className="btn btn-green btn-block">Learn More</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-xl-3 px-5">
+            <div className="row align-items-center mb-5">
+              <div className="col-12 col-md-3 col-xl-12">
+                <div className="p-5 p-md-0 text-center">
+                  <img src="/img/icons/4p-homepage-yourbuilding.png" className="img-fluid w-75" alt="" />
+                </div>
+              </div>
+              <div className="col-12 col-md-9 col-xl-12 text-xl-center">
+                <h4>Your Building</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione praesentium veritatis at error fuga magni labore cum iusto, natus obcaecati recusandae, fugiat voluptatibus reprehenderit laborum ex magnam molestiae? Sit, omnis!</p>
+                <p><a href="/" className="btn btn-green btn-block">Learn More</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-xl-3 px-5">
+            <div className="row align-items-center mb-5">
+              <div className="col-12 col-md-3 col-xl-12">
+                <div className="p-5 p-md-0 text-center">
+                  <img src="/img/icons/4p-homepage-yourcommunity.png" className="img-fluid w-75" alt="" />
+                </div>
+              </div>
+              <div className="col-12 col-md-9 col-xl-12 text-xl-center">
+                <h4>Your Community</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione praesentium veritatis at error fuga magni labore cum iusto, natus obcaecati recusandae, fugiat voluptatibus reprehenderit laborum ex magnam molestiae? Sit, omnis!</p>
+                <p><a href="/" className="btn btn-green btn-block">Learn More</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-xl-3 px-5">
+            <div className="row align-items-center mb-5">
+              <div className="col-12 col-md-3 col-xl-12">
+                <div className="p-5 p-md-0 text-center">
+                  <img src="/img/icons/4p-homepage-yourbusiness.png" className="img-fluid w-75" alt="" />
+                </div>
+              </div>
+              <div className="col-12 col-md-9 col-xl-12 text-xl-center">
+                <h4 className="mb-3">Your Business</h4>
+                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p><a href="/" className="btn btn-green btn-block">Learn More</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Community
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import HowItWorks from "./home/HowItWorks"
 import Testimonials from './home/Testimonials'
 import ImageBar from './home/ImageBar'
 import EnrichYourself from './home/EnrichYourself'
+import Community from './home/Community'
 
 
 
@@ -56,59 +57,7 @@ const IndexPage = () => (
 
     <EnrichYourself />
 
-    <section id="bring-us-to-your-community" className="bg-primary text-white">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-lg-6 mx-lg-auto text-center">
-            <h3 className="text-white">Bring 4p to your community</h3>
-            <p>Supportive Sub-Headline Here</p>
-            <p>4p Foods sources farm-fresh food from farms in the Washington DC local foodshed and delivers them to your office or apartment building once each week.</p>
-          </div>
-        </div>
-        <div className="responsive-card-deck cards-lg-4 cards-md-2">
-          <div className="card bg-transparent text-center">
-            <img src="https://via.placeholder.com/500x350" className="img-fluid" alt="" />
-            <div className="card-body p-0">
-              <h5 className="text-white my-3">Your<br className="d-none d-lg-block" />Office</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quam quod error fugiat veritatis dicta soluta dolore? Perspiciatis sed obcaecati</p>
-            </div>
-            <div className="card-footer bg-transparent">
-              <a href="#" className="btn btn-green-dark">See Newsletter</a>
-            </div>
-          </div>
-          <div className="card bg-transparent text-center">
-            <img src="https://via.placeholder.com/500x350" className="img-fluid" alt="" />
-            <div className="card-body p-0">
-              <h5 className="text-white my-3">Your<br className="d-none d-lg-block" />Office</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quam quod error fugiat veritatis dicta soluta dolore? Perspiciatis sed obcaecati</p>
-            </div>
-            <div className="card-footer bg-transparent">
-              <a href="#" className="btn btn-green-dark">See Newsletter</a>
-            </div>
-          </div>
-          <div className="card bg-transparent text-center">
-            <img src="https://via.placeholder.com/500x350" className="img-fluid" alt="" />
-            <div className="card-body p-0">
-              <h5 className="text-white my-3">Your<br className="d-none d-lg-block" />Office</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quam quod error fugiat veritatis dicta soluta dolore? Perspiciatis sed obcaecati</p>
-            </div>
-            <div className="card-footer bg-transparent">
-              <a href="#" className="btn btn-green-dark">See Newsletter</a>
-            </div>
-          </div>
-          <div className="card bg-transparent text-center">
-            <img src="https://via.placeholder.com/500x350" className="img-fluid" alt="" />
-            <div className="card-body p-0">
-              <h5 className="text-white my-3">Your<br className="d-none d-lg-block" />Office</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quam quod error fugiat veritatis dicta soluta dolore? Perspiciatis sed obcaecati</p>
-            </div>
-            <div className="card-footer bg-transparent">
-              <a href="#" className="btn btn-green-dark">See Newsletter</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Community />
 
     <img src="https://via.placeholder.com/1800x600" className="img-fluid" alt="" />
 

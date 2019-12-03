ba37c4cbfd0cb5740b3bff4fea1ae81efc375116
tombeck <t@ocupop.com>
2019-12-03 09:44:23 -0800

    added placholder page templates to match the instances required in content

---
+++ src/templates/food-access-programs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+function FoodAccessPrograms() {
+  return (
+    <div>
+        <h1>Food Access Programs</h1>
+    </div>
+  )
+}
+
+FoodAccessPrograms.propTypes = {
+
+}
+
+export default FoodAccessPrograms
+
---
+++ src/templates/our-partners.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const OurPartners = props => {
+  return (
+    <div>
+      <h1>Our Partners</h1>
+    </div>
+  )
+}
+
+OurPartners.propTypes = {
+
+}
+
+export default OurPartners

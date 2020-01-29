a80c21d3326f8f6201ad1c2fbb69344da14d6c53
Cody Nicoll <cdnicoll@gmail.com>
2020-01-29 14:12:18 -0800

    Fix to add firebase back into gatspy project

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,6 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const fbCreds = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email:process.env.FIREBASE_CLIENT_EMAIL,
-  client_id:process.env.FIREBASE_CLIENT_ID,
-  auth_uri:process.env.FIREBASE_AUTH_URI,
-  token_uri:process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url:process.env.FIREBASE_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url:process.env.FIREBASE_X509_CERT_URL,
-}
-
-// console.log(fbCreds)
-
 module.exports = {
   siteMetadata: {
     title: `4pFoods Website`,
@@ -140,8 +125,18 @@ module.exports = {
     {
       resolve: 'gatsby-source-firestore',
       options: {
-        credential: fbCreds,
-        // credential: require('./firebaseKey.json'),
+        credential: {
+          type: process.env.FIREBASE_TYPE,
+          project_id: process.env.FIREBASE_PROJECT_ID,
+          private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.FIREBASE_PRIVATE_KEY,
+          client_email:process.env.FIREBASE_CLIENT_EMAIL,
+          client_id:process.env.FIREBASE_CLIENT_ID,
+          auth_uri:process.env.FIREBASE_AUTH_URI,
+          token_uri:process.env.FIREBASE_TOKEN_URI,
+          auth_provider_x509_cert_url:process.env.FIREBASE_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url:process.env.FIREBASE_X509_CERT_URL,
+        },
         databaseURL:process.env.FIREBASE_DATABASE_URL,
         types: [
           {
@@ -152,7 +147,6 @@ module.exports = {
               category: doc.category,
               department: doc.department,
               variants: doc.variants
-              // author___NODE: doc.author.id,
             }),
           },
         ],

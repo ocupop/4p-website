ca9f3e0a09682002acac6e5c38de46d0393cec43
Cody Nicoll <cdnicoll@gmail.com>
2020-01-29 14:34:43 -0800

    formatting cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,14 +130,15 @@ module.exports = {
           project_id: process.env.FIREBASE_PROJECT_ID,
           private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
           private_key: process.env.FIREBASE_PRIVATE_KEY,
-          client_email:process.env.FIREBASE_CLIENT_EMAIL,
-          client_id:process.env.FIREBASE_CLIENT_ID,
-          auth_uri:process.env.FIREBASE_AUTH_URI,
-          token_uri:process.env.FIREBASE_TOKEN_URI,
-          auth_provider_x509_cert_url:process.env.FIREBASE_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url:process.env.FIREBASE_X509_CERT_URL,
+          client_email: process.env.FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.FIREBASE_CLIENT_ID,
+          auth_uri: process.env.FIREBASE_AUTH_URI,
+          token_uri: process.env.FIREBASE_TOKEN_URI,
+          auth_provider_x509_cert_url:
+            process.env.FIREBASE_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: process.env.FIREBASE_X509_CERT_URL,
         },
-        databaseURL:process.env.FIREBASE_DATABASE_URL,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
         types: [
           {
             type: 'Products',
@@ -146,7 +147,7 @@ module.exports = {
               name: doc.name,
               category: doc.category,
               department: doc.department,
-              variants: doc.variants
+              variants: doc.variants,
             }),
           },
         ],

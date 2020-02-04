0ce10cbcc10d24a66097e55bf484974f76cf5e92
Cody Nicoll <cdnicoll@gmail.com>
2020-02-04 08:14:01 -0800

    Adding redux actions

--- src/components/cart/useCart.js
+++ src/components/cart/useCart.js
@@ -6,29 +6,30 @@ import {
   isEmpty
 } from 'react-redux-firebase'
 import { useSelector } from 'react-redux'
+import { addToCart, removeFromCart } from './cartActions'
 import { asyncActionStart, asyncActionFinish, asyncActionError } from '../../common/async/asyncActions'
 
 const [items, addItem] = useState([]);
 
 function useCart({ author }) {
-  // const firestore = useFirestore()
+  const firestore = useFirestore()
 
-  // useFirestoreConnect([
-  //   {
-  //     collection: 'quotes',
-  //     where: ['author.value', '==', author.id]
-  //   }
-  // ])
+  useFirestoreConnect([
+    {
+      collection: 'cart',
+      where: ['author.value', '==', author.id]
+    }
+  ])
 
-  function addToCart(item) {
-
+  function addItemToCart(item) {
+    // addToCart(item);
   }
 
-  function removeFromCart(item) {
-
+  function removeItemFromCart(item) {
+    // removeFromCart(item);
   }
 
-  return { items, addToCart, removeFromCart }
+  return { items, addItemToCart, removeItemFromCart }
 }
 
 export default useCart;

7bc5123ba0876ca8f2ca3bd48d5ddc38d3fbfd15
tombeck <t@ocupop.com>
2020-02-16 09:31:21 -0800

    updated config for testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -171,6 +171,16 @@ module.exports = {
           client_x509_cert_url: process.env.GATSBY_FIREBASE_X509_CERT_URL,
         },
         databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+        // appConfig: {
+        //   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+        //   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+        //   databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+        //   projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+        //   storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+        //   messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+        //   appID: process.env.GATSBY_FIREBASE_APP_ID,
+        //   measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID
+        // },
         types: [
           {
             type: 'Product',

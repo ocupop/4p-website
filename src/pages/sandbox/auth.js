85396cfd8abb9c83a974375d7f7e0771af295ac0
tombeck <t@ocupop.com>
2020-02-19 05:08:03 -0800

    auth sandbox

--- src/pages/sandbox/auth.js
+++ src/pages/sandbox/auth.js
@@ -1,51 +1,49 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useFirebase, isLoaded, isEmpty } from 'react-redux-firebase'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-// import { useHistory } from 'react-router-dom'; // if you use react-router
-// import GoogleButton from 'react-google-button' // optional
 import LoadingComponent from '../../common/ui/LoadingComponent'
+import { openModal } from '../../features/modal/modalActions'
 
 function AuthPage() {
   const firebase = useFirebase()
+  const dispatch = useDispatch()
+  // const firestore = useFirestore()
   const auth = useSelector(state => state.firebase.auth)
+  // const profile = useSelector(state => state.firestore.profile)
+
 
   return (
     <section>
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <StyledFirebaseAuth
-              uiConfig={{
-                signInFlow: 'popup',
-                signInSuccessUrl: '/sandbox/auth',
-                signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-                callbacks: {
-                  signInSuccessWithAuthResult: (authResult) => {
-                    // @TODO: Refactor our auth component to use the result details
-                    console.log(authResult)
-
-                    firebase.handleRedirectResult(authResult).then(() => {
-                      // history.push(redirectUrl); if you use react router to redirect
-                    });
-                    return false;
-                  },
-                },
-              }}
-              firebaseAuth={firebase.auth()}
-            />
-
-            <h2>Auth Details</h2>
+            <h2>FIrestore: Profile</h2>
+            <div>{JSON.stringify(profile, null, 2)}</div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-12">
+            <h2>Firebase: Auth</h2>
             {!isLoaded(auth) ? (
               <LoadingComponent inverted />
             ) : (isEmpty(auth) ? (
               <>
                 <h3>You are not signed in</h3>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => dispatch(openModal('LoginModal'))}
+                >Login
+                </button>
               </>
             ) : (
                 <>
-                  <h3>Congrats! You are in</h3>
-                  <button type="button" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+                  <p>Congrats! You are in</p>
+                  <div className="mb-5">
+                    <button className="btn btn-warning" type="button" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+                  </div>
+
                   {JSON.stringify(auth, null, 2)}
 
                 </>
--- src/store/defaultConfig.js
+++ src/store/defaultConfig.js
@@ -11,12 +11,20 @@ import { setAnalyticsUser } from '../common/utils/analytics'
 export const defaultRRFConfig = {
   userProfile: 'users', // root that user profiles are written to
   updateProfileOnLogin: true, // enable/disable updating of profile on login
-  presence: 'presence', // list currently online users under "presence" path in RTDB
+  // presence: 'presence', // list currently online users under "presence" path in RTDB
   sessions: null, // Skip storing of sessions
   useFirestoreForProfile: true, // Save profile to Firestore instead of Real Time Database
   useFirestoreForStorageMeta: true, // Metadata associated with storage file uploads goes to Firestore
+  // profileFactory: (userData, profileData, firestore) => { // how profiles are stored in database
+  //   const { user } = userData
+  //   return {
+  //     email: user.email
+  //   }
+  // },
   onAuthStateChanged: (auth, firebaseInstance, dispatch) => {
     if (auth) {
+      // const { currentUser } = firebaseInstance.auth()
+      // console.log("CURRENT:", currentUser.displayName)
       // Set auth within error handler
       // setErrorUser(auth)
       // Initalize messaging with dispatch

1b5798d143b4c2fe9d7c664dcb6583018e80b10b
Cody Nicoll <cdnicoll@gmail.com>
2020-02-26 15:28:05 -0800

    Updating product cart

--- src/features/product/ProductCard.jsx
+++ src/features/product/ProductCard.jsx
@@ -22,8 +22,7 @@ const ProductCard = ({ product }) => {
         {vendor.name && <h6>{vendor.name}</h6>}
         <div className="d-flex align-items-center justify-content-between w-100">
           <div className="product-amount">
-            {defaultVariant.size && defaultVariant.size}
-            {defaultVariant.label && <em>{defaultVariant.label}</em>}
+            {defaultVariant.size && defaultVariant.size} {defaultVariant.label && <em>{defaultVariant.label}</em>}
           </div>
           <div className="product-price">{formatMoney(defaultVariant.price)}</div>
         </div>

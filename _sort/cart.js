25965cde02538085c7b43dfd377579c5e1286ef3
Jon Burr <jonathanwburr@gmail.com>
2020-04-15 10:17:29 -0700

    remove gatsby project from codebase (migrated to 4p-customer repo)

--- _sort/cart.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CartPage = () => (
-  <>
-    <h1>Add Cart Page</h1>
-  </>
-)
-
-export default CartPage
--- src/common/constants/BagTypes.js
+++
@@ -1,3 +0,0 @@
-export const RECURRING_BAG = 'recurring'
-export const SINGLE_BAG = 'single'
-export const AUTOMATED_BAG = 'automated'
